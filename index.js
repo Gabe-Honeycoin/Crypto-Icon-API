6229ebee6669a4749a5715dd3a1e27955b253af6
Gabriel <gabriel.mbugua307@gmail.com>
2022-05-10 09:38:53 +0300

    Dynamic avatars added\

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require("express");
+var fs = require("fs"),
+  request = require("request");
+var unirest = require("unirest");
 var bodyParser = require("body-parser");
 const { dirname } = require("path");
-const fs = require("fs");
 const port = process.env.PORT || 3000;
 
 var app = express();
@@ -11,24 +13,47 @@ var dotenv = require("dotenv").config({
 });
 
 app.use(bodyParser.json()); // for parsing application/json
+app.use(
+  bodyParser.raw({
+    type: "image/png",
+    limit: "10mb",
+  })
+);
 
 const path = dirname(require.main.filename) + "/icon/";
 
-app.get("/get-icon/:icon", function (req, res) {
+app.get("/get-icon/:icon", function (req, resp) {
   const icon = req.params.icon;
   try {
     const pngPath = `${path}/${icon}.png`;
     if (fs.existsSync(pngPath)) {
-      res.sendFile(pngPath);
+      resp.sendFile(pngPath);
     } else {
-      res.sendFile(`${path}_no_image_.png`);
+      download(
+        `https://ui-avatars.com/api/?background=random&name=${icon}`,
+        "temp.png",
+        function () {
+          console.log("done");
+
+          resp.sendFile(`${dirname(require.main.filename)}/temp.png`);
+        }
+      );
     }
   } catch (err) {
-    res.status(403).json({
+    resp.status(403).json({
       error: err,
     });
   }
 });
+
+var download = function (uri, filename, callback) {
+  request.head(uri, function (err, res, body) {
+    console.log("content-type:", res.headers["content-type"]);
+    console.log("content-length:", res.headers["content-length"]);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on("close", callback);
+  });
+};
 
 app.listen(port, () => {
   console.log(`listening at https://cryptoiconapi.honeycoin.app/:${port}`);
